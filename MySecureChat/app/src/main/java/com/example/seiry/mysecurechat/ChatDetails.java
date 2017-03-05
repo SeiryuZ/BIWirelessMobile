0fb4487cb244cc13abce256bdc9a39640eb73555
Steven <stvn.bu@gmail.com>
2017-03-06 06:46:23 +0700

    Add Tutorial Project

---
+++ MySecureChat/app/src/main/java/com/example/seiry/mysecurechat/ChatDetails.java
@@ -0,0 +1,13 @@
+package com.example.seiry.mysecurechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_details);
+    }
+}
